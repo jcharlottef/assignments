f6d56f245c18911a17e910db8ec3ac0895dc49c4
jcharlottef <jcfaselt@gmail.com>
2020-01-11 13:48:47 -0500

    debugging

--- calculator.py
+++ calculator.py
@@ -1,10 +1,10 @@
 def multiply(a,b):
     return a * b
-def add(a,b)
+def add(a,b):
 	return a + b
-def subtract(a,b)
+def subtract(a,b):
 	return a - b
-def divide(a,b)
+def divide(a,b):
 	return a / b
 
 print("I'm going use the calculator functions to multiply 10 and 2")
