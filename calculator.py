18d01296ace9d9f79de70e8c738b4e08529c326b
Jackie <59415930+jcharlottef@users.noreply.github.com>
2020-01-20 22:27:36 -0500

    adding comments to code
    adding comments to code; cleaning it up

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+#definding the functions of the calculator
+
 def multiply(a,b):
     return a * b
 def add(a,b):
@@ -6,11 +8,17 @@ def subtract(a,b):
 	return a - b
 def divide(a,b):
 	return a / b
+
+# bonus: create a sq function
 def sqaure(a):
 	return a**2
 
-
+#printing mult 
 print("I'm going use the calculator functions to multiply 10 and 2")
+y = multiply(10,2)
+print(y)
+
+#priting a sq
 x = sqaure(5)
 print(x)
 
